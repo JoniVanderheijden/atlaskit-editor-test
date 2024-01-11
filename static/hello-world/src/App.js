ef9f9741ad6a0010c588b644a85654c31b3a0a02
Joni Vanderheijden <jvanderheijden@atlassian.com>
2024-01-11 21:17:16 +1100

    Set up basic editor

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -1,20 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { invoke } from '@forge/bridge';
-import {Editor} from "@atlaskit/editor-core";
+import React from 'react';
 
-function App() {
-  const [data, setData] = useState(null);
+import { EditorContext, WithEditorActions } from '@atlaskit/editor-core';
+import { ComposableEditor } from '@atlaskit/editor-core/composable-editor';
+import { createDefaultPreset } from '@atlaskit/editor-core/preset-default';
 
-  useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-  }, []);
+const CommentEditor = () => {
+  const presetProps = {
+    props: {},
+  };
+  const defaultPreset = createDefaultPreset(presetProps);
+  const initialADFDocument = {
+    version: 1,
+    type: 'doc',
+    content: [
+      {
+        type: 'paragraph',
+        content: [{ type: 'text', text: 'Hello World!' }],
+      },
+    ],
+  };
 
   return (
-    <div>
-      {data ? data : 'Loading...'}
-      <Editor/>
-    </div>
+    <EditorContext>
+      <WithEditorActions
+        render={(editorActions) => (
+          <ComposableEditor
+            preset={defaultPreset}
+            defaultValue={initialADFDocument}
+            onChange={async () => console.log('The current doc is: ', await editorActions.getValue())}
+          />
+        )}
+      />
+    </EditorContext>
   );
-}
+};
 
-export default App;
+export default CommentEditor;
